1d9f6ed391c7e43b14b91e91b27ac38165672f43
Ushabandaru1 <85926933+Ushabandaru1@users.noreply.github.com>
2021-07-02 22:48:39 +0530

    Add files via upload

---
+++ P_file13.java
@@ -0,0 +1,13 @@
+package patterns;
+public class P_file13 {
+	public static void main(String[] args) {
+		for(int i = 1; i<=5; i++)
+		{
+			for(char j = 'E'; j>='A'; j--)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+}

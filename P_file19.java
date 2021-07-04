0c79638ab2ab6ee43e4640d06fff1e03c9fbb597
Ushabandaru1 <85926933+Ushabandaru1@users.noreply.github.com>
2021-07-04 21:30:14 +0530

    Add files via upload

---
+++ P_file19.java
@@ -0,0 +1,13 @@
+package patterns;
+public class P_file19 {
+	public static void main(String[] args) {
+		for (int i = 5; i >= 1; i--)
+		{
+			for(int j = i; j <= 5; j++)
+			{
+				System.out.print(j);;
+			}
+			System.out.println();
+		}
+	}
+}
